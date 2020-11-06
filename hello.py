06e30384e533b73c041f0d8727635680d7d8f62e
slowsnow <shebarshina.ira@yandex.ru>
2020-11-06 16:41:46 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 16:35:00 2020
+
+@author: sheba
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
